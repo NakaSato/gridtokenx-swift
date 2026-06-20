78febc749fac32cc1e780f2796150304a05300b4
WiT <wit.chanthawat@gmail.com>
2026-06-20 16:06:43 +0700

    Initial Commit

---
+++ gridtokexios/gridtokexiosApp.swift
@@ -0,0 +1,32 @@
+//
+//  gridtokexiosApp.swift
+//  gridtokexios
+//
+//  Created by Chanthawat Kiriyadee on 20/6/2569 BE.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct gridtokexiosApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ gridtokexiosTests/gridtokexiosTests.swift
@@ -0,0 +1,19 @@
+//
+//  gridtokexiosTests.swift
+//  gridtokexiosTests
+//
+//  Created by Chanthawat Kiriyadee on 20/6/2569 BE.
+//
+
+import Testing
+@testable import gridtokexios
+
+struct gridtokexiosTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
